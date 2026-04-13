b77f4fc177c1c0e8566bfdf9e0f993fadb42dcdd
daen <29579547+dkalphacoder@users.noreply.github.com>
2026-04-13 18:22:33 -0400

    added bio to user entity

--- apps/backend/models/user.go
+++ apps/backend/models/user.go
@@ -12,6 +12,7 @@ type User struct {
 	Username     string    `json:"username"      gorm:"size:50;not null;unique"`
 	Email        string    `json:"email"         gorm:"size:255;not null;unique"`
 	PasswordHash string    `json:"-"             gorm:"not null"`
+	Bio          *string   `json:"bio"`
 	CreatedAt    time.Time `json:"created_at"`
 	UpdatedAt    time.Time `json:"updated_at"`
 }
